5d1e1383a41b85a672599ba5bee011da6e16c2bd
Niek Nijland <ngnijland@gmail.com>
2021-10-15 16:38:57 +0200

    fix: make bin command work

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
+
 import { program } from "commander";
 
 program
   .description("An application to lint your TODO comments")
-  .version("0.3.0");
+  .version("0.3.0")
+  .parse(process.argv);
