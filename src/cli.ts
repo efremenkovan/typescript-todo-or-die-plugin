f4f08f64c867d499e40fb25a6f15877f8078c73d
Niek Nijland <ngnijland@gmail.com>
2021-10-15 21:40:37 +0200

    feat: add dir arg and dynamic version

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,17 @@
 
 import { program } from "commander";
 
+type PackageJSON = {
+  version: `${number}.${number}.${number}`;
+};
+
+const { version }: PackageJSON = require("../package.json");
+
 program
   .description("An application to lint your TODO comments")
-  .version("0.3.0")
+  .version(version)
+  .argument("[dir]", "Folder to check for comments")
+  .action((dir) => {
+    console.log(dir);
+  })
   .parse(process.argv);
